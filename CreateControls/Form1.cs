69d8e7aa22556d214386184a34446bd9e4288833
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-04-05 00:09:32 +0300

    reverting.

---
+++ CreateControls/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateControls
+{
+    public partial class createControls : Form
+    {
+        public createControls()
+        {
+            InitializeComponent();
+        }
+
+        private void clearControlsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
