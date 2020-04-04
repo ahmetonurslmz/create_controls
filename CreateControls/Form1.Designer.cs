ccb52aefdc715b1b1728f40d94f0d7d478931663
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-04-04 19:58:28 +0300

    completes create controls tool.

--- CreateControls/Form1.Designer.cs
+++ CreateControls/Form1.Designer.cs
@@ -29,116 +29,99 @@
         private void InitializeComponent()
         {
             this.grpControls = new System.Windows.Forms.GroupBox();
+            this.panelNumberOfControls = new System.Windows.Forms.Panel();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtNumberOfCols = new System.Windows.Forms.TextBox();
+            this.txtNumberOfRows = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.clearControlsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearControls = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitApplication = new System.Windows.Forms.ToolStripMenuItem();
             this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.textBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.labelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtNumberOfRows = new System.Windows.Forms.TextBox();
-            this.txtNumberOfCols = new System.Windows.Forms.TextBox();
-            this.btnCreate = new System.Windows.Forms.Button();
-            this.grpControls.SuspendLayout();
+            this.createTextBoxControls = new System.Windows.Forms.ToolStripMenuItem();
+            this.createLabelControls = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panelNumberOfControls.SuspendLayout();
             this.menuStrip1.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpControls
             // 
-            this.grpControls.Controls.Add(this.panel1);
             this.grpControls.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.grpControls.Location = new System.Drawing.Point(0, 27);
             this.grpControls.Name = "grpControls";
-            this.grpControls.Size = new System.Drawing.Size(331, 421);
+            this.grpControls.Size = new System.Drawing.Size(482, 497);
             this.grpControls.TabIndex = 0;
             this.grpControls.TabStop = false;
             this.grpControls.Text = "Controls";
             // 
-            // menuStrip1
+            // panelNumberOfControls
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.createToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(331, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
+            this.panelNumberOfControls.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelNumberOfControls.Controls.Add(this.btnCreate);
+            this.panelNumberOfControls.Controls.Add(this.txtNumberOfCols);
+            this.panelNumberOfControls.Controls.Add(this.txtNumberOfRows);
+            this.panelNumberOfControls.Controls.Add(this.label4);
+            this.panelNumberOfControls.Controls.Add(this.label3);
+            this.panelNumberOfControls.Controls.Add(this.label1);
+            this.panelNumberOfControls.Location = new System.Drawing.Point(123, 27);
+            this.panelNumberOfControls.Name = "panelNumberOfControls";
+            this.panelNumberOfControls.Size = new System.Drawing.Size(238, 154);
+            this.panelNumberOfControls.TabIndex = 3;
+            this.panelNumberOfControls.Visible = false;
             // 
-            // fileToolStripMenuItem
+            // btnCreate
             // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.clearControlsToolStripMenuItem,
-            this.exitToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCreate.Location = new System.Drawing.Point(69, 116);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(92, 35);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
-            // clearControlsToolStripMenuItem
+            // txtNumberOfCols
             // 
-            this.clearControlsToolStripMenuItem.Name = "clearControlsToolStripMenuItem";
-            this.clearControlsToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+Shift+C";
-            this.clearControlsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
-            | System.Windows.Forms.Keys.C)));
-            this.clearControlsToolStripMenuItem.Size = new System.Drawing.Size(223, 22);
-            this.clearControlsToolStripMenuItem.Text = "Clear Controls";
-            this.clearControlsToolStripMenuItem.Click += new System.EventHandler(this.clearControlsToolStripMenuItem_Click);
+            this.txtNumberOfCols.Location = new System.Drawing.Point(168, 78);
+            this.txtNumberOfCols.MaxLength = 1;
+            this.txtNumberOfCols.Name = "txtNumberOfCols";
+            this.txtNumberOfCols.Size = new System.Drawing.Size(26, 20);
+            this.txtNumberOfCols.TabIndex = 4;
+            this.txtNumberOfCols.Text = "0";
             // 
-            // exitToolStripMenuItem
+            // txtNumberOfRows
             // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+Shift+E";
-            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
-            | System.Windows.Forms.Keys.E)));
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(223, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            this.txtNumberOfRows.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtNumberOfRows.Location = new System.Drawing.Point(168, 49);
+            this.txtNumberOfRows.MaxLength = 1;
+            this.txtNumberOfRows.Name = "txtNumberOfRows";
+            this.txtNumberOfRows.Size = new System.Drawing.Size(26, 20);
+            this.txtNumberOfRows.TabIndex = 3;
+            this.txtNumberOfRows.Text = "0";
             // 
-            // createToolStripMenuItem
+            // label4
             // 
-            this.createToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.textBoxToolStripMenuItem,
-            this.labelToolStripMenuItem});
-            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
-            this.createToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
-            this.createToolStripMenuItem.Text = "Create";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(12, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Number of Cols:";
             // 
-            // textBoxToolStripMenuItem
+            // label3
             // 
-            this.textBoxToolStripMenuItem.Name = "textBoxToolStripMenuItem";
-            this.textBoxToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+Shift+T";
-            this.textBoxToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
-            | System.Windows.Forms.Keys.T)));
-            this.textBoxToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
-            this.textBoxToolStripMenuItem.Text = "Text Box";
-            // 
-            // labelToolStripMenuItem
-            // 
-            this.labelToolStripMenuItem.Name = "labelToolStripMenuItem";
-            this.labelToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+Shift+L";
-            this.labelToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
-            | System.Windows.Forms.Keys.L)));
-            this.labelToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
-            this.labelToolStripMenuItem.Text = "Label";
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Controls.Add(this.btnCreate);
-            this.panel1.Controls.Add(this.txtNumberOfCols);
-            this.panel1.Controls.Add(this.txtNumberOfRows);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(50, 53);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(238, 154);
-            this.panel1.TabIndex = 0;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Number of Rows:";
             // 
             // label1
             // 
@@ -150,63 +133,91 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Number of Controls";
             // 
-            // label3
+            // menuStrip1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 49);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(149, 18);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Number of Rows:";
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.createToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(482, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // label4
+            // fileToolStripMenuItem
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 81);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(141, 18);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Number of Cols:";
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearControls,
+            this.exitApplication});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
             // 
-            // txtNumberOfRows
+            // clearControls
             // 
-            this.txtNumberOfRows.Location = new System.Drawing.Point(168, 42);
-            this.txtNumberOfRows.Name = "txtNumberOfRows";
-            this.txtNumberOfRows.Size = new System.Drawing.Size(26, 27);
-            this.txtNumberOfRows.TabIndex = 3;
+            this.clearControls.Name = "clearControls";
+            this.clearControls.ShortcutKeyDisplayString = "Ctrl+Shift+C";
+            this.clearControls.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.C)));
+            this.clearControls.Size = new System.Drawing.Size(223, 22);
+            this.clearControls.Text = "Clear Controls";
+            this.clearControls.Click += new System.EventHandler(this.clearControlsToolStripMenuItem_Click);
             // 
-            // txtNumberOfCols
+            // exitApplication
             // 
-            this.txtNumberOfCols.Location = new System.Drawing.Point(168, 74);
-            this.txtNumberOfCols.Name = "txtNumberOfCols";
-            this.txtNumberOfCols.Size = new System.Drawing.Size(26, 27);
-            this.txtNumberOfCols.TabIndex = 4;
+            this.exitApplication.Name = "exitApplication";
+            this.exitApplication.ShortcutKeyDisplayString = "Ctrl+Shift+E";
+            this.exitApplication.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.E)));
+            this.exitApplication.Size = new System.Drawing.Size(223, 22);
+            this.exitApplication.Text = "Exit";
+            this.exitApplication.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
-            // btnCreate
+            // createToolStripMenuItem
             // 
-            this.btnCreate.Location = new System.Drawing.Point(69, 116);
-            this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(92, 35);
-            this.btnCreate.TabIndex = 1;
-            this.btnCreate.Text = "Create";
-            this.btnCreate.UseVisualStyleBackColor = true;
+            this.createToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createTextBoxControls,
+            this.createLabelControls});
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.createToolStripMenuItem.Text = "Create";
+            // 
+            // createTextBoxControls
+            // 
+            this.createTextBoxControls.Name = "createTextBoxControls";
+            this.createTextBoxControls.ShortcutKeyDisplayString = "Ctrl+Shift+T";
+            this.createTextBoxControls.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.T)));
+            this.createTextBoxControls.Size = new System.Drawing.Size(190, 22);
+            this.createTextBoxControls.Text = "Text Box";
+            this.createTextBoxControls.Click += new System.EventHandler(this.createTextBoxControls_Click);
+            // 
+            // createLabelControls
+            // 
+            this.createLabelControls.Name = "createLabelControls";
+            this.createLabelControls.ShortcutKeyDisplayString = "Ctrl+Shift+L";
+            this.createLabelControls.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.L)));
+            this.createLabelControls.Size = new System.Drawing.Size(190, 22);
+            this.createLabelControls.Text = "Label";
+            this.createLabelControls.Click += new System.EventHandler(this.createLabelControls_Click);
             // 
             // createControls
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(331, 450);
+            this.ClientSize = new System.Drawing.Size(482, 517);
+            this.Controls.Add(this.panelNumberOfControls);
             this.Controls.Add(this.grpControls);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "createControls";
             this.Text = "Create Controls";
-            this.grpControls.ResumeLayout(false);
+            this.panelNumberOfControls.ResumeLayout(false);
+            this.panelNumberOfControls.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -217,18 +228,19 @@
         private System.Windows.Forms.GroupBox grpControls;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem clearControlsToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearControls;
+        private System.Windows.Forms.ToolStripMenuItem exitApplication;
         private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem textBoxToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem labelToolStripMenuItem;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem createTextBoxControls;
+        private System.Windows.Forms.ToolStripMenuItem createLabelControls;
+        private System.Windows.Forms.Panel panelNumberOfControls;
         private System.Windows.Forms.Button btnCreate;
         private System.Windows.Forms.TextBox txtNumberOfCols;
         private System.Windows.Forms.TextBox txtNumberOfRows;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
