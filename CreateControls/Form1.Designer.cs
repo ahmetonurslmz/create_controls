2f454dada755d12915f43dc17c2eeddb5dfe9ac0
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-04-04 13:43:34 +0300

    creates control group box.

--- CreateControls/Form1.Designer.cs
+++ CreateControls/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.grpControls = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // grpControls
+            // 
+            this.grpControls.Location = new System.Drawing.Point(0, 21);
+            this.grpControls.Name = "grpControls";
+            this.grpControls.Size = new System.Drawing.Size(331, 427);
+            this.grpControls.TabIndex = 0;
+            this.grpControls.TabStop = false;
+            this.grpControls.Text = "Controls";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(331, 450);
+            this.Controls.Add(this.grpControls);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpControls;
     }
 }
 
