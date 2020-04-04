7912a2f93a5561340dcad4ebd8de7a6abaed6987
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-04-04 13:57:00 +0300

    creates create control panel.

--- CreateControls/Form1.Designer.cs
+++ CreateControls/Form1.Designer.cs
@@ -36,11 +36,21 @@
             this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.textBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.labelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNumberOfRows = new System.Windows.Forms.TextBox();
+            this.txtNumberOfCols = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.grpControls.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpControls
             // 
+            this.grpControls.Controls.Add(this.panel1);
             this.grpControls.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.grpControls.Location = new System.Drawing.Point(0, 27);
             this.grpControls.Name = "grpControls";
@@ -116,6 +126,71 @@
             this.labelToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
             this.labelToolStripMenuItem.Text = "Label";
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.btnCreate);
+            this.panel1.Controls.Add(this.txtNumberOfCols);
+            this.panel1.Controls.Add(this.txtNumberOfRows);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(50, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(238, 154);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(27, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Controls";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Number of Rows:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 18);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Number of Cols:";
+            // 
+            // txtNumberOfRows
+            // 
+            this.txtNumberOfRows.Location = new System.Drawing.Point(168, 42);
+            this.txtNumberOfRows.Name = "txtNumberOfRows";
+            this.txtNumberOfRows.Size = new System.Drawing.Size(26, 27);
+            this.txtNumberOfRows.TabIndex = 3;
+            // 
+            // txtNumberOfCols
+            // 
+            this.txtNumberOfCols.Location = new System.Drawing.Point(168, 74);
+            this.txtNumberOfCols.Name = "txtNumberOfCols";
+            this.txtNumberOfCols.Size = new System.Drawing.Size(26, 27);
+            this.txtNumberOfCols.TabIndex = 4;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(69, 116);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(92, 35);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            // 
             // createControls
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -127,8 +202,11 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "createControls";
             this.Text = "Create Controls";
+            this.grpControls.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,6 +222,13 @@
         private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem textBoxToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem labelToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtNumberOfCols;
+        private System.Windows.Forms.TextBox txtNumberOfRows;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
